61b1807be43e1ed6bc40a57712088ca4a892ed9f
MAHENDRA SAI DIVI <92313437+MAHENDRA9535@users.noreply.github.com>
2023-04-14 12:26:13 +0530

    Add files via upload

---
+++ java mini project/matrix.java
@@ -0,0 +1,111 @@
+import java.util.*;
+
+public class matrix {
+    public static Scanner sc = new Scanner(System.in);
+    static int mat[][], adj[][], trans[][];
+    static int row, col;
+    static int det;
+
+    int[][] enter() {
+        System.out.print("\nEnter the no. of Rows to the Matrix: ");
+        row = sc.nextInt();
+        System.out.print("\nEnter the no. of Columns to the Matrix: ");
+        col = sc.nextInt();
+        mat = new int[row][col];
+        System.out.println("\nEnter Elements to the Matrix\n");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print("Enter a number: ");
+                mat[i][j] = sc.nextInt();
+            }
+        }
+        return mat;
+    }
+
+    void determinant(int x[][], int a, int b) {
+        int temp[][] = new int[a][2 * b];
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                temp[i][j] = x[i][j];
+            }
+        }
+        for (int i = 0; i < a; i++) {
+            for (int j = b, j2 = 0; j < (2 * b) && j2 < b; j++, j2++) {
+                temp[i][j] = x[i][j2];
+            }
+        }
+
+        /*
+         * for(int i=0;i<a;i++)
+         * {
+         * for(int j=0;j<2*b;j++)
+         * {
+         * System.out.print(temp[i][j]+"\t");
+         * }
+         * System.out.println();
+         * }
+         * System.out.println("\n");
+         */
+
+        for (int i = 0; i < b; i++) {
+            int prod = 1;
+            for (int j = 0; j < a; j++) {
+                prod *= temp[j][i + j];
+                // System.out.println(temp[j][i+j]);
+            }
+            // System.out.println("---"+prod);
+            det += prod;
+        }
+
+        System.out.println("\n");
+
+        for (int i = b; i < (2 * b); i++) {
+            int prod = 1;
+            for (int j = 0; j < a; j++) {
+                prod *= temp[j][i - j];
+                // System.out.println(temp[j][i-j]);
+            }
+            // System.out.println("---"+prod);
+            det -= prod;
+        }
+        System.out.println("The Determinant is: " + det);
+    }
+
+    void transpose(int[][] mat1, int r1, int c1) {
+        trans = new int[col][row];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                trans[j][i] = mat[i][j];
+            }
+        }
+    }
+
+    void display(int x[][], int a, int b) {
+        System.out.println("\nThe Elements of the matrix are.\n");
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                System.out.print(x[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        System.out.println("\n");
+    }
+
+    public static void main(String args[]) {
+        matrix ob1 = new matrix();
+        ob1.enter();
+        ob1.display(mat, row, col);
+        ob1.determinant(mat, row, col);
+        // ob1.transpose();
+        // ob1.display(trans,col,row);
+    }
+
+    public void addition(int[][] mat1, int r1, int c1, int[][] mat2, int r2, int c2) {
+    }
+
+    public void subtraction(int[][] mat1, int r1, int c1, int[][] mat2, int r2, int c2) {
+    }
+
+    public void multiplication(int[][] mat1, int r1, int c1, int[][] mat2, int r2, int c2) {
+    }
+}
